ee6cb374ef46e355eea50083ebcae0dea339f2b4
Simankin <Simankin@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2018-02-20 18:15:18 +0000

    Остальные тесты для Task и Assigenee (добавление, удаление и обновление)

---
+++ trunk/src/test/java/TaskManagerMain/TaskManager/server/modelServer/TaskManagerModelImplUpdateTaskTest.java
@@ -0,0 +1,33 @@
+package TaskManagerMain.TaskManager.server.modelServer;
+
+import TaskManagerMain.TaskManager.common.entity.Entity;
+import TaskManagerMain.TaskManager.common.entity.Task;
+import TaskManagerMain.TaskManager.common.entity.TaskImpl;
+import TaskManagerMain.TaskManager.common.service.JDBCDaoTask;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class TaskManagerModelImplUpdateTaskTest {
+
+    Task task = new TaskImpl("1", "2", "3", "4", "4");
+    JDBCDaoTask dao = mock(JDBCDaoTask.class);
+    TaskManagerModel model = new TaskManagerModelImpl(dao, dao);
+
+    @Captor
+    ArgumentCaptor<TaskImpl> entityArgumentCaptor = ArgumentCaptor.forClass(TaskImpl.class);;
+
+    @Test
+    public void testUpdateTask() {
+
+        model.updateTask(task);
+
+        verify(dao).update(entityArgumentCaptor.capture());
+        verify(dao, atLeastOnce()).update((Entity) task);
+
+    }
+}
