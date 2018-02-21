7542d12614b9b0683a6c85ba3f10d22acaf9ace6
Simankin <Simankin@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2018-02-21 07:08:27 +0000

    Инициализация вынесена в тестах в отдельный метод

--- src/test/java/TaskManagerMain/TaskManager/server/modelServer/TaskManagerModelImplAddTaskTest.java
+++ src/test/java/TaskManagerMain/TaskManager/server/modelServer/TaskManagerModelImplAddTaskTest.java
@@ -1,14 +1,11 @@
 package TaskManagerMain.TaskManager.server.modelServer;
 
 import TaskManagerMain.TaskManager.common.entity.*;
-import TaskManagerMain.TaskManager.common.service.GenericDao;
-import TaskManagerMain.TaskManager.common.service.JDBCDao;
+import TaskManagerMain.TaskManager.common.service.JDBCDaoAssignee;
 import TaskManagerMain.TaskManager.common.service.JDBCDaoTask;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.*;
-import org.mockito.internal.matchers.Equals;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.testng.annotations.Test;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
@@ -16,9 +13,17 @@ import static org.mockito.Mockito.verify;
 
 public class TaskManagerModelImplAddTaskTest {
 
-    Task task = new TaskImpl("1", "2", "3", "4", "4");
-    JDBCDaoTask dao = mock(JDBCDaoTask.class);
-    TaskManagerModel model = new TaskManagerModelImpl(dao, dao);
+    Task task;
+    JDBCDaoTask dao;
+    TaskManagerModel model;
+
+    @Before
+    public void inicialize(){
+        task = new TaskImpl("1", "2", "3", "4", "4");
+        dao = mock(JDBCDaoTask.class);
+        model = new TaskManagerModelImpl(dao, dao);
+
+    }
 
     @Captor
     ArgumentCaptor<TaskImpl> entityArgumentCaptor = ArgumentCaptor.forClass(TaskImpl.class);;
--- src/test/java/TaskManagerMain/TaskManager/server/modelServer/TaskManagerModelImplUpdateTaskTest.java
+++ src/test/java/TaskManagerMain/TaskManager/server/modelServer/TaskManagerModelImplUpdateTaskTest.java
@@ -4,19 +4,26 @@ import TaskManagerMain.TaskManager.common.entity.Entity;
 import TaskManagerMain.TaskManager.common.entity.Task;
 import TaskManagerMain.TaskManager.common.entity.TaskImpl;
 import TaskManagerMain.TaskManager.common.service.JDBCDaoTask;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.testng.annotations.Test;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-
 public class TaskManagerModelImplUpdateTaskTest {
 
-    Task task = new TaskImpl("1", "2", "3", "4", "4");
-    JDBCDaoTask dao = mock(JDBCDaoTask.class);
-    TaskManagerModel model = new TaskManagerModelImpl(dao, dao);
+    Task task;
+    JDBCDaoTask dao;
+    TaskManagerModel model;
+
+    @Before
+    public void inicialize(){
+        task = new TaskImpl("1", "2", "3", "4", "4");
+        dao = mock(JDBCDaoTask.class);
+        model = new TaskManagerModelImpl(dao, dao);
+
+    }
 
     @Captor
     ArgumentCaptor<TaskImpl> entityArgumentCaptor = ArgumentCaptor.forClass(TaskImpl.class);;
