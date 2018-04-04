26ebad1bad78e946ebbc3aa16ab4906f3f7e2235
Simankin <Simankin@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2018-04-04 13:59:48 +0000

    packaging war

--- trunk/server/src/main/java/labs/taskmanger/server/mainServer/ServerTaskManager.java
+++ trunk/server/src/main/java/labs/taskmanger/server/mainServer/ServerTaskManager.java
@@ -4,9 +4,7 @@ package labs.taskmanger.server.mainServer;
 import com.google.inject.Guice;
 import labs.taskmanger.common.service.ServiceModule;
 import labs.taskmanger.server.controllerServer.ControllerModule;
-import labs.taskmanger.server.controllerServer.TaskManagerController;
 import labs.taskmanger.server.modelServer.ModelModule;
-import labs.taskmanger.server.modelServer.TaskManagerModel;
 import labs.taskmanger.server.viewServer.TaskManagerView;
 import labs.taskmanger.server.viewServer.ViewModule;
 
